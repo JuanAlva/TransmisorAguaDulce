a040d999196ae7762bfe3e70b646315dfa555c6d
juanalva <73912276+juanalva@users.noreply.github.com>
2024-12-23 23:53:51 -0500

    Calibracion con tabla

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,24 @@
 #define SLEEP_DURATION_US 5000000  // 2.5 segundos en microsegundos
 #define SAMPLES_CNT 64         // Número de muestras para suavizado
 
+// Tabla de calibración (valores reales vs valores crudos del ADC)
+float voltageReal[] = {0.487, 0.546, 0.604, 0.663, 0.722, 0.78, 0.838, 0.896, 0.956, 1.013, 1.071, 1.13, 1.188, 1.246, 1.305, 1.363, 1.421, 1.48, 1.538, 1.596, 1.653};  // Voltajes reales
+float currentReal[] = {4, 4.48, 4.96, 5.44, 5.92, 6.4, 6.88, 7.36, 7.84, 8.32, 8.8, 9.28, 9.76, 10.24, 10.72, 11.2, 11.68, 12.16, 12.64, 13.12, 13.6};  // Corrientes reales
+int adcRaw[] = {240, 302, 340, 387, 435, 483, 530, 585, 637, 692, 742, 797, 853, 904, 956, 1016, 1080, 1138, 1196, 1257, 1350};        // Valores crudos del ADC
+const int calibrationPoints = 21;                  // Numero de puntos en la tabla
+float voltageCalib = 0.0;
+float currentCalib = 0.0;
+
 // Valores para el mapeo
 #define ADC_MIN 480//410//549//480//448            // Valor ADC para 0.66 V
-#define ADC_MAX 850//1152//810//713//800//808            //780            // Valor ADC para 2.45 V
+#define ADC_MAX 850//1152//810//713//800//808            //780            // Valor ADC para 2.45 V  .
 #define VOLTAGE_MIN 0.787//0.49866//0.864//0.787//0.487      // Voltaje mínimo esperado
 #define VOLTAGE_MAX 1.205//2.44890//1.1550//1.051//1.151//1.160      //1.101      // Voltaje máximo esperado
-#define ADC_UMBRAL_MIN 448
-#define ADC_UMBRAL_MAX 1271
+
+// Variable global para almacenar el último valor válido del ADC
+int previous_valid_adc = adcRaw[0];  // Inicializado al valor mínimo esperado
+#define ADC_UMBRAL_MIN adcRaw[0]//480//448 // Corresponde a 0% de 4-20 mA. 0 mA
+#define ADC_UMBRAL_MAX adcRaw[21]//1226//1271 // Corresponde a 33% de 4-20 mA, 0.01456 mA
 
 
 // Valores para mapeo de presión
@@ -104,6 +115,35 @@ float map_current_to_m3d(float current) {
     return M3_MIN + (M3_MAX - M3_MIN) * ((current - CURRENT_MIN_M3) / (CURRENT_MAX_M3 - CURRENT_MIN_M3));
 }
 
+float calibrateADC(int adcValue) {
+  if (adcValue < ADC_UMBRAL_MIN || adcValue > ADC_UMBRAL_MAX) {
+    Serial.printf("ADC fuera de rango: %d. Usando último valor válido: %d\n", adcValue, previous_valid_adc);
+    adcValue = previous_valid_adc;
+    return adcValue;
+  }
+
+  else {
+    for (int i = 0; i < calibrationPoints - 1; i++) {
+      if (adcValue >= adcRaw[i] && adcValue <= adcRaw[i+1]) {
+        voltageCalib = voltageReal[i] + (voltageReal[i+1] - voltageReal[i]) * (adcValue - adcRaw[i]) / (adcRaw[i+1] - adcRaw[i]);
+        previous_valid_adc = adcValue;
+        return voltageCalib;
+      }
+    }
+  }
+}
+
+
+float calibrateCurrentADC(int adcValue) {
+    for (int i = 0; i < calibrationPoints - 1; i++) {
+      if (adcValue >= adcRaw[i] && adcValue <= adcRaw[i+1]) {
+        currentCalib = currentReal[i] + (currentReal[i+1] -currentReal[i]) * (adcValue - adcRaw[i]) / (adcRaw[i+1] - adcRaw[i]);
+        previous_valid_adc = adcValue;
+        return currentCalib;
+      }
+    }
+}
+
 // Inicializa el módulo LoRa
 void initLoRa() {
     Serial.println("Inicializando módulo LoRa...");
@@ -127,7 +167,7 @@ void enterLowPowerMode() {
 void setup() {
     Serial.begin(115200);
     analogReadResolution(12);
-    analogSetAttenuation(ADC_6db); //ADC_11db
+    analogSetAttenuation(ADC_11db); //ADC_6db
 
     esp_task_wdt_init(WATCHDOG_TIMEOUT, true);
     esp_task_wdt_add(NULL);
@@ -138,31 +178,21 @@ void setup() {
     initLoRa();
 }
 
-// Variable global para almacenar el último valor válido del ADC
-int previous_valid_adc = ADC_MIN;  // Inicializado al valor mínimo esperado
-
 void loop() {
     esp_task_wdt_reset();
 
     // Leer y procesar el valor ADC
     int adc_value = read_adc(ADC_CHANNEL);
 
-    // Validar el valor del ADC
-    if (adc_value < ADC_UMBRAL_MIN || adc_value > ADC_UMBRAL_MAX) {
-        Serial.printf("ADC fuera de rango: %d. Usando último valor válido: %d\n", adc_value, previous_valid_adc);
-        adc_value = previous_valid_adc;  // Usa el último valor válido
-        delay(500);  // Pausa para estabilizar la señal
-    } else {
-        previous_valid_adc = adc_value;  // Actualiza el último valor válido
-    }
-
-    float voltage = map_adc_to_voltage(adc_value);
-    float filtered_voltage = kalmanFilter(voltage);  // Aplicar filtro de Kalman
-    float current = filtered_voltage / 0.1221;
+    float calibrated_voltage = calibrateADC(adc_value);
+    // float voltage = map_adc_to_voltage(adc_value);
+    //float filtered_voltage = kalmanFilter(calibrated_voltage);  // Aplicar filtro de Kalman
+    float value_current = calibrateCurrentADC(adc_value);
+    float current = kalmanFilter(value_current);
     float pressure = map_current_to_pressure(current);
-    float mca = map_current_to_mca(current) / 100;
-    float m3 = mca * 12.267; // Area de tanque de agua m2 (aprox)
-    float m3d = map_current_to_m3d(current);
+    float mca = pressure * 55.8111; //0.704;   // map_current_to_mca(current) / 100;
+    float m3 = mca * 12.267 / 100; // Area de tanque de agua m2 (aprox)
+    float m3d = m3;//float m3d = map_current_to_m3d(pressure);
 
     // Crear objeto JSON
     StaticJsonDocument<128> doc;
@@ -175,8 +205,8 @@ void loop() {
     serializeJson(doc, jsonBuffer);
 
     // Imprimir resultados
-    Serial.printf("\nADC Value: %d, Voltage: %.3f V, Filtered Voltage: %.3f V, Current: %.3f mA, Pressure: %.3f psi, Metros Columna de Agua: %.3f cm, Metros Cubico: %.3f m3, Metros Cubico Directo: %.3f m3\n",
-                  adc_value, voltage, filtered_voltage, current, pressure, mca, m3, m3d);
+    Serial.printf("\nADC Value: %d, Voltage: %.3f V, Current: %.3f mA, Pressure: %.3f psi, Metros Columna de Agua: %.3f cm, Metros Cubico: %.3f m3\n",
+                  adc_value, calibrated_voltage, current, pressure, mca, m3);
     Serial.println("Enviando Paquete...");
 
     // Intentar enviar paquete con reintentos
