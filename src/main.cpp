6fb7ff3e7d60fe42274121a692d44e526a607279
juanalva <73912276+juanalva@users.noreply.github.com>
2025-03-22 07:13:20 -0500

    Error 3 m3

--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,28 @@
 #include <WiFi.h>
 #include <esp_bt.h>
 #include <esp_system.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
 
-#define ADC_CHANNEL 32
-#define SAMPLES_CNT 8192
+#define SAMPLES_CNT 32
 
 #define SS      5   // Pin NSS (CS) del modulo LoRa
 #define RST     14  // Pin RESET del modulo LoRa
 #define DIO0    4   // Pin DIO0 del modulo LoRa
 #define BAND    433E6 // Frecuencia LoRa
 
-float voltageReal[] = {0.7245, 0.8036, 0.959, 1.2557, 1.2896};//{0.487, 0.546, 0.604, 0.663, 0.722, 0.78, 0.864, 0.896, 0.956, 1.013, 1.071, 1.128, 1.188, 1.246, 1.305};
-int adcRaw[] = {295, 362, 470, 745, 774};//{260, 312, 350, 397, 445, 493, 526, 595, 647, 702, 752, 781, 863, 914, 966};
-const int calibrationPoints = 5;// 15;
+float voltageReal[] = {4.15, 8, 12, 12.5};
+int adcRaw[] = {10535, 20244, 30307, 31566};
+const int calibrationPoints = 4;
 float voltageCalib = 0.0;
 
 #define ADC_UMBRAL_MIN adcRaw[0]
 #define ADC_UMBRAL_MAX adcRaw[calibrationPoints-1]
 
 #define RETRY_LIMIT 3
-#define WATCHDOG_TIMEOUT 30
+#define WATCHDOG_TIMEOUT 10
 
-float x_est = 0.0;
-float P_est = 1.0;
-float Q = 0.1;
-float R = 0.3;
-float K = 0.0;
+Adafruit_ADS1115 ads; // Crear objeto ADS1115
 
 void disableWiFiAndBluetooth() {
     WiFi.mode(WIFI_OFF);
@@ -38,37 +35,7 @@ void disableWiFiAndBluetooth() {
     btStop();
 }
 
-struct KalmanOutput {
-    float x_est;
-    float P_est;
-    float K;
-};
-
-KalmanOutput kalmanFilter(float z_measured) {
-    // Predicción
-    float x_pred = x_est;
-    float P_pred = P_est + Q;
-
-    // Ganancia de Kalman
-    float K = P_pred / (P_pred + R);
-
-    // Corrección
-    x_est = x_pred + K * (z_measured - x_pred);
-    P_est = (1 - K) * P_pred;
-
-    // Retornar los valores
-    return {x_est, P_est, K};
-}
-
-int read_adc(int channel) {
-    int adc_value = 0;
-    for (int i = 0; i < SAMPLES_CNT; i++) {
-        adc_value += analogRead(channel);
-    }
-    return adc_value / SAMPLES_CNT;
-}
-
-float calibrateVoltageADC(int adcValue) {
+float calibrater(int adcValue) {
     if (adcValue < adcRaw[0]) return voltageReal[0];
     if (adcValue > adcRaw[calibrationPoints - 1]) return voltageReal[calibrationPoints - 1];
     
@@ -80,6 +47,10 @@ float calibrateVoltageADC(int adcValue) {
         }
     }
     return voltageCalib;
+}
+
+float normalizeCurrent(float current_mA) {
+    return (current_mA - 4.0) / (20.0 - 4.0);
 }
 
 void initLoRa() {
@@ -101,33 +72,53 @@ void initLoRa() {
     LoRa.setSignalBandwidth(125E3); // Ancho de banda (125 kHz)
     LoRa.setCodingRate4(5);  // Coding Rate 4/5
     Serial.println("LoRa Inicializado Correctamente!");
+    //delay(100);
 }
 
 void setup() {
     Serial.begin(115200);
-    analogReadResolution(12);
-    analogSetAttenuation(ADC_11db);
     // Inicializar el watchodg con tiempo limite de 10 segundos
     esp_task_wdt_init(WATCHDOG_TIMEOUT, true);
     esp_task_wdt_add(NULL); // Se aplica al loop principal
     disableWiFiAndBluetooth();
     initLoRa();
+    
+    Wire.begin();  // Iniciar I2C en ESP32 (SDA=21, SCL=22 por defecto)
+
+    if (!ads.begin()) {
+        Serial.println("¡Error! No se encontró el ADS1115 en la dirección 0x48.");
+        while (1);
+    }
+    
+    Serial.println("ADS1115 detectado en la dirección 0x48.");
+    ads.setGain(GAIN_TWO);  // Configurar ganancia ±4.096V (1 bit = 0.125mV)
 }
 
 unsigned long lastSampleTime = 0;
 const unsigned long sampleInterval = 1000;
+unsigned long lastLoRaResetTime = 0; // Variable para rastrear el tiempo del último reinicio de LoRa
 
 void loop() {
+    int retryCount = 0;
+    int16_t rawValue = ads.readADC_SingleEnded(0);
+    
+    if (rawValue == 0x8000) { // Detecta error en la lectura
+        Serial.println("Error en la lectura del ADC.");
+        return;
+    }
+
     if (millis() - lastSampleTime >= sampleInterval) {
         lastSampleTime = millis();
-        int adc_value = read_adc(ADC_CHANNEL); // Lectura del ADc
-        if (adc_value < ADC_UMBRAL_MIN || adc_value > ADC_UMBRAL_MAX) {
+        rawValue = ads.readADC_SingleEnded(0);
+        if (rawValue < ADC_UMBRAL_MIN || rawValue > ADC_UMBRAL_MAX) {
             Serial.println("Medición fuera de rango. Descartando datos.");
             return;
         }
-        float calibrated_voltage = calibrateVoltageADC(adc_value);  // Calibracion del Voltaje del ADc
-        KalmanOutput result = kalmanFilter(calibrated_voltage); // Aplicacion del Filtro de Kalman
-        float mtr = calibrated_voltage * 360.54 - 220.7; // Conversion a Metro columna de agua
+        rawValue = ads.readADC_SingleEnded(0);
+        float voltage = rawValue * 2.048 / 32768.0;  // Conversión correcta
+        float calibrateSignal = calibrater(rawValue);  // Calibracion del Voltaje del ADC
+        float normalizedValue = normalizeCurrent(calibrateSignal);
+        float mtr = normalizedValue * 669.754;//360.54 - 220.7; // Conversion a Metro columna de agua
         float m3d = mtr * 0.1227 + 0.0298; // Conversion a Metros cubicos
         
         // Formateo en JSON
@@ -137,26 +128,45 @@ void loop() {
         serializeJson(doc, jsonBuffer);
 
         // Imprimir datos en serial
-        Serial.printf("\nADC Value: %d, Voltage: %.3f V, kalman: %.3f V, Metros Columna de Agua: %.3f cm, Metros Cubico: %.3f m3\n", adc_value, calibrated_voltage, result.x_est, mtr, m3d);
+        Serial.printf("\nADC: %d, Voltaje: %.3f V, Corriente: %.3f mA, Normal: %.3f, Metros Columna de Agua: %.3f cm, Metros Cubico: %.3f m3\n", rawValue, voltage, calibrateSignal, normalizedValue, mtr, m3d);
         Serial.println("Enviando Paquete...");
-        Serial.printf("Kalman Output -> x_est: %.3f V, P_est: %.5f, K: %.5f\n", 
-            result.x_est, result.P_est, result.K);
         
         LoRa.idle();
         LoRa.beginPacket();
         LoRa.print(jsonBuffer);
-        if (LoRa.endPacket() == 0) {
-            Serial.println("Error: No se pudo enviar el paquete. Reiniciando módulo LoRa...");
+        
+        retryCount = 0;
+        while (retryCount < RETRY_LIMIT) {
+            if (LoRa.endPacket() == 0) {
+                Serial.println("Error en el envío. Reintentando...");
+                delay(500);
+                retryCount++;
+            } else {
+                Serial.println("Paquete enviado correctamente");
+                break;
+            }
+        }
+
+        if (retryCount == RETRY_LIMIT) {
+            Serial.println("Error persistente. Reiniciando módulo LoRa...");
             LoRa.end();
             delay(1000);
-            esp_task_wdt_delete(NULL); // Pausar watchdog temporalmente
             initLoRa();
-            esp_task_wdt_add(NULL); // Reactivar watchdog
-        } else {
-            Serial.println("Paquete enviado correctamente");
+            lastLoRaResetTime = millis();  // Actualiza el tiempo del último reinicio
+        }
+        
+        // **Reinicio automático cada 10 segundos**
+        if (millis() - lastLoRaResetTime >= 600000) {
+            Serial.println("Reiniciando módulo LoRa por mantenimiento preventivo...");
+            LoRa.end();
+            delay(1000);
+            initLoRa();
+            delay(500); // Espera para estabilizar
+            ads.readADC_SingleEnded(0); // Lectura en vacío para estabilizar
+            lastLoRaResetTime = millis();  // Actualiza el tiempo del último reinicio
         }
         
         // Alimentar el watchdog
-        //esp_task_wdt_reset();
+        esp_task_wdt_reset();
     }
 }
